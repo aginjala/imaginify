bb65f7f5f96724c8c401e7930b15be86a665d1e5
Ashok Ginjala <ashok.ginjala@nuance.com>
2024-02-19 00:11:21 -0500

    initial transformation form

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import Header from "@/components/shared/Header";
+import { transformationTypes } from "../../../../../../constants";
+import TransformationForm from "@/components/shared/TransformationForm";
 
-const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+  const transformationType = transformationTypes[type];
+  return (
+    <>
+      <Header
+        title={transformationType.title}
+        subtitle={transformationType.subTitle}
+      />
+      <TransformationForm />
+    </>
+  );
 };
 
 export default AddTransformationTypePage;
